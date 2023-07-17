493acb1cf5a1aedb7baef363380ca27e0d01a95d
AhmadAllan <aaa200265@gmail.com>
2023-07-17 19:36:55 +0300

    test

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,6 +12,79 @@ const Header = () => {
       <nav className="container mx-auto flex items-center justify-between p-4">
         <Link to="/">CodePulse</Link> {/* Logo or site name */}
         <ul className="flex items-center space-x-4">
+          
+           {/* Add the routes as nav elements */}
+           <li>
+            <Link
+              to="/dashboard"
+              className={`text-gray-400 ${
+                isActive('/dashboard') ? 'text-white border-b-2 border-white' : '' // Add 'text-white' class and underline for active link
+              }`}
+            >
+              <span>Dashboard</span> {/* Link text */}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/projects"
+              className={`text-gray-400 ${
+                isActive('/projects') ? 'text-white border-b-2 border-white' : '' // Add 'text-white' class and underline for active link
+              }`}
+            >
+              <span>Projects</span> {/* Link text */}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/codeEditor"
+              className={`text-gray-400 ${
+                isActive('/codeEditor') ? 'text-white border-b-2 border-white' : '' // Add 'text-white' class and underline for active link
+              }`}
+            >
+              <span>Code Editor</span> {/* Link text */}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/versionControl"
+              className={`text-gray-400 ${
+                isActive('/versionControl') ? 'text-white border-b-2 border-white' : '' // Add 'text-white' class and underline for active link
+              }`}
+            >
+              <span>Version Control</span> {/* Link text */}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/chat"
+              className={`text-gray-400 ${
+                isActive('/chat') ? 'text-white border-b-2 border-white' : '' // Add 'text-white' class and underline for active link
+              }`}
+            >
+              <span>Chat</span> {/* Link text */}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/codeReview"
+              className={`text-gray-400 ${
+                isActive('/codeReview') ? 'text-white border-b-2 border-white' : '' // Add 'text-white' class and underline for active link
+              }`}
+            >
+              <span>Code Review</span> {/* Link text */}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/taskManagement"
+              className={`text-gray-400 ${
+                isActive('/taskManagement') ? 'text-white border-b-2 border-white' : '' // Add 'text-white' class and underline for active link
+              }`}
+            >
+              <span>Task Management</span> {/* Link text */}
+            </Link>
+          </li>
+
           <li>
             <Link
               to="/login"
